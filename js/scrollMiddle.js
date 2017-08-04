71979663b7ab2b6478ac4d1fbb169731512f3184
Tom Hyatt <info@tomhyatt.co.uk>
2017-08-04 01:13:50 +0100

    Webpage WIP
    Work in progress Desk website

---
+++ js/scrollMiddle.js
@@ -0,0 +1,3 @@
+window.onload = function () {
+  window.scrollTo((document.body.offsetWidth - document.documentElement.offsetWidth) / 2, (document.body.offsetHeight - document.documentElement.offsetHeight) / 2);
+};
