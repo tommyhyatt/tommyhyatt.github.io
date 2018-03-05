bbc4b98217fd03eb07ee488681ac000d5403f4cf
Tom Hyatt <info@tomhyatt.co.uk>
2018-03-05 18:52:03 +0000

    Deleting old website
    Moved responsive image map site to Codepen, to make way for new website

--- js/scrollMiddle.js
+++
@@ -1,3 +0,0 @@
-window.onload = function () {
-  window.scrollTo((document.body.offsetWidth - document.documentElement.offsetWidth) / 2, (document.body.offsetHeight - document.documentElement.offsetHeight) / 2);
-};
