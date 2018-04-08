2238fb68c87f6193aba1a14e40b5a03489f05062
Tom Hyatt <info@tomhyatt.co.uk>
2018-04-08 10:40:08 +0100

    .

--- js/centered.js
+++ js/centered.js
@@ -1,1 +1,16 @@
-window.scrollTo((document.body.offsetWidth )/2,0)
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+
+var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width,
+    windowWidth = getWidth();
+
+
+
+window.scrollTo((windowWidth - screenWidth)/2,0);
