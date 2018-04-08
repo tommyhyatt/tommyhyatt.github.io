f12bd67631847b4df308216bd466e6433807b0c0
Tom Hyatt <info@tomhyatt.co.uk>
2018-04-08 19:16:29 +0100

    .

--- js/screensaver.js
+++ js/screensaver.js
@@ -1,6 +1,6 @@
 var screensaver = document.getElementById("screensaverContainer");
 
-setTimeout(function(){ screensaver.style.display = "block" }, 10000);
+setTimeout(function(){ screensaver.style.display = "block" }, 15000);
 
 screensaver.addEventListener('mouseover', function() { screensaver.style.display = "none";setTimeout(function(){ screensaver.style.display = "block" }, 10000); });
 
