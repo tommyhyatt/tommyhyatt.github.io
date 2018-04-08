883085a3450431ec2448a81b861fec43503ad210
Tom Hyatt <info@tomhyatt.co.uk>
2018-04-08 10:45:11 +0100

    .

--- js/centered.js
+++ js/centered.js
@@ -8,9 +8,21 @@ function getWidth() {
   );
 }
 
+function getHeight() {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
+}
+
 var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width,
-    windowWidth = getWidth();
+    windowWidth = getWidth(),
+    screenHeight = (window.innerHeight > 0) ? window.innerHeight : screen.height,
+    windowHeight = getHeight();
 
 
 
-window.scrollTo((windowWidth - screenWidth)/2,1000);
+window.scrollTo((windowWidth - screenWidth)/2,(windowHeight - screenHeight)/2);
