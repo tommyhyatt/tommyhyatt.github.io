b5301d64080770858ac29a036d74f7e3df66a4a0
Tom Hyatt <info@tomhyatt.co.uk>
2018-04-11 23:27:48 +0100

    .

--- js/screensaver.js
+++ js/screensaver.js
@@ -1,11 +1,12 @@
 var screensaver = document.getElementById("screensaverContainer"),
-    screen = document.getElementById("screen");
+    screen = document.getElementById("screen"),
+    tutorial = document.getElementById("screenTutorial");
 
-setTimeout(function(){ screensaver.style.display = "block" }, 15000);
+setTimeout(function(){ screensaver.style.display = "block"; }, 30000);
 
 screensaver.addEventListener('mouseover', function() { screensaver.style.display = "none";});
 
-screen.addEventListener('mouseout', function() { setTimeout(function(){ screensaver.style.display = "block" }, 10000); });
+screen.addEventListener('mouseout', function() { setTimeout(function(){ screensaver.style.display = "block"; }, 10000); });
 
 
 
