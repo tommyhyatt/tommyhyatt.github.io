50bdef1f378b97ab23babe08af2087f872eb99ff
Tom Hyatt <hyatt.tom@googlemail.com>
2024-09-13 12:57:10 +0100

    zoom

---
+++ script2.js
@@ -0,0 +1,18 @@
+function zoomIn() {
+    
+    var zoom = document.getElementsByTagName('html')[0];
+    var screenSaver = document.getElementById('screensaver');
+   // zoom.style.fontSize = 'clamp(0px, 1.8vh, 25px)';
+    setTimeout(() => {
+                    zoom.style.fontSize = 'clamp(0px, 1.8vh, 25px)';
+                }, 1500);
+    
+    setTimeout(() => {
+                    screensaver.style.opacity = '0';
+                }, 6000);
+    
+    setTimeout(() => {
+                    screensaver.style.display = 'none';
+                }, 7000);
+    
+}
