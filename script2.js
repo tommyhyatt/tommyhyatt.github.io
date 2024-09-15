27d87e0bb25b7283b5102fc522206d3b45341924
Tom Hyatt <hyatt.tom@googlemail.com>
2024-09-15 14:47:23 +0100

    disabling animation

--- script2.js
+++ script2.js
@@ -1,20 +1,19 @@
 function zoomIn() {
     
-    var zoom = document.getElementsByTagName('html')[0];
-    var screenSaver = document.getElementById('screensaver');
-   // zoom.style.fontSize = 'clamp(0px, 1.8vh, 25px)';
-    setTimeout(() => {
-                    zoom.style.fontSize = 'clamp(0px, 1.6vh, 25px)';
-                }, 1500);
-    
-    setTimeout(() => {
-                    screensaver.style.opacity = '0';
-                }, 6000);
-    
-    setTimeout(() => {
-                    screensaver.style.display = 'none';
-                }, 7000);
-    setTimeout(() => {
-                    zoom.style.transition = 'unset';
-                }, 7000);
+//    var zoom = document.getElementsByTagName('html')[0];
+//    var screenSaver = document.getElementById('screensaver');
+//    setTimeout(() => {
+//                    zoom.style.fontSize = 'clamp(0px, 1.6vh, 25px)';
+//                }, 1500);
+//    
+//    setTimeout(() => {
+//                    screensaver.style.opacity = '0';
+//                }, 6000);
+//    
+//    setTimeout(() => {
+//                    screensaver.style.display = 'none';
+//                }, 7000);
+//    setTimeout(() => {
+//                    zoom.style.transition = 'unset';
+//                }, 7000);
 }
